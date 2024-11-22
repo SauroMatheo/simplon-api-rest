76cc8bf0f6f518da5ad174e0717d9205ff167deb
SauroMatheo <sauromatheo@gmail.com>
2024-11-22 13:48:16 +0100

    Squashed commit of the following:
    commit 9c97b7b994b91fa4104ab2c65c39c09ed9476758
    Author: SauroMatheo <sauromatheo@gmail.com>
    Date:   Fri Nov 22 13:37:14 2024 +0100
    
        Equipe joueur nullable
    
    commit d7244adecfbf7d9d92a8c2588900e7b05ac4d695
    Author: SauroMatheo <sauromatheo@gmail.com>
    Date:   Fri Nov 22 13:24:02 2024 +0100
    
        Fix Player
    
    commit 3799f2124928c98262e0b54338188053696fd6cb
    Author: SauroMatheo <sauromatheo@gmail.com>
    Date:   Fri Nov 22 09:20:12 2024 +0100
    
        Retiré les constantes
    
        Des fois elles fonctionnent, des fois non, donc autant les retirer

--- src/Controller/APIJoueursController.php
+++ src/Controller/APIJoueursController.php
@@ -6,44 +6,199 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Routing\Attribute\Route;
 
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Doctrine\ORM\EntityManagerInterface;
 
 use App\Repository\JoueursRepository;
+use App\Repository\EquipesRepository;
+use App\Entity\Joueurs;
+
 
 #[Route('/api/joueurs')]
 class APIJoueursController extends AbstractController
 {
-    #[Route('/get', name: 'get_joueurs', methods: ['GET'])]
-    public function getJoueur(JoueursRepository $joueursRepository, SerializerInterface $serializer): JsonResponse
+    #[Route('', name: 'get_joueurs', methods: ['GET'])]
+    public function getJoueurs(
+        Request $request,
+        JoueursRepository $joueursRepo,
+        SerializerInterface $serializer
+        ): JsonResponse
     {
-        $joueurs = $joueursRepository->findAll();
-        $joueursJson = $serializer->serialize($joueurs, 'json', ['groups' => 'joueursMinimum']);
+        $id = $request->get('id');
+
+        if (isset($id)) {
+            $joueur = $joueursRepo->find($id);
+
+            if ($joueur === null) {
+                return $this->renvoiJson(Response::HTTP_NOT_FOUND, "Entité introuvable");
+            }
+
+            $joueursJson = $serializer->serialize($joueur, 'json', ['groups' => 'joueur']);
+
+        } else {
+            $joueurs = $joueursRepo->findAll();
+            $joueursJson = $serializer->serialize($joueurs, 'json', ['groups' => 'joueursMinimum']);
+        }
 
         return new JsonResponse($joueursJson, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/get/{id}', name: 'get_joueur_id', methods: ['GET'])]
-    public function getJoueurId(JoueursRepository $joueursRepository, SerializerInterface $serializer, int $id): Response
+
+    #[Route('', name: 'supprimer_joueur', methods: ['DELETE'])]
+    public function supprimer(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        JoueursRepository $joueursRepo
+        ): JsonResponse
     {
-        $joueur = $joueursRepository->find($id);
+        if (!$request->query->has('id')) {
+            return $this->renvoiJson(Response::HTTP_BAD_REQUEST, "Aucun id spécifié");
+        }
+
+        try {
+            $id = $request->get('id');
+            $joueur = $joueursRepo->find($id);
+        } catch (Exception $e) {
+            return $this->renvoiJson(Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+
+        if ($joueur === null) {
+            return $this->renvoiJson(Response::HTTP_NOT_FOUND, "Entité introuvable");
+        }
+
+        $entityManager->remove($joueur);
+        $entityManager->flush();
+
+        if (null !== $joueursRepo->find($id)) {
+            return $this->renvoiJson(Response::HTTP_INTERNAL_SERVER_ERROR, "L'entité n'a pas pu être supprimée");
+        }
+
+        return $this->renvoiJson(Response::HTTP_NO_CONTENT, "Joueur supprimé avec succès");
+    }
+
+
+    #[Route('', name: 'ajouter_joueur', methods: ['POST'])]
+    public function ajouter(
+        Request $request,
+        EquipesRepository $equipeRepo,
+        EntityManagerInterface $entityManager,
+        SerializerInterface $serializer
+        ): JsonResponse
+    {
+        $requestArray = $request->toArray();
+
+        try {
+            // TODO: Re-modifier pour utiliser le deserializer en créant un Normalizer
+            if (!isset($requestArray["nom"]) || !isset($requestArray["prenom"])) {
+                return $this->renvoiJson(Response::HTTP_BAD_REQUEST, "Le nom ou prenom est manquant");
+            }
+
+            $joueur = new Joueurs();
+            $joueur->setNom($requestArray["nom"]);
+            $joueur->setPrenom($requestArray["prenom"]);
+            
+            // Un joueur n'a pas forcément une equipe
+            if (isset($requestArray["equipe"]) && gettype($requestArray["equipe"]) == "integer") {
+                $joueur->setEquipe(
+                    $equipeRepo->find($requestArray["equipe"])
+                );
+            }
+
+        } catch (Exception $e) {
+            return $this->renvoiJson(Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+
+        $entityManager->persist($joueur);
+        $entityManager->flush();
+
+
+        return new JsonResponse(
+            $serializer->serialize($joueur, 'json', ['groups' => 'joueursMinimum']),
+            Response::HTTP_CREATED,
+            [], true
+        );
+    }
+
+
+    #[Route('', name: 'update_joueur', methods: ['PUT'])]
+    public function modifier(
+        Request $request,
+        EntityManagerInterface $em,
+        JoueursRepository $joueurRepo,
+        EquipesRepository $equipeRepo,
+        SerializerInterface $serializer
+        ): JsonResponse
+    {
+        $json = json_decode($request->getContent(), true);
+
+        if (!isset($json['id'])) {
+            return $this->renvoiJson(Response::HTTP_BAD_REQUEST, "Aucun id spécifié");
+        }
+
+        try {
+            $joueur = $joueurRepo->find($json['id']);
+        } catch (Exception $e) {
+            return $this->renvoiJson(Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+
+        if ($joueur === null) {
+            return $this->renvoiJson(Response::HTTP_NOT_FOUND, "Joueur introuvable");
+        }
+
+        try {
+            // TODO: Re-modifier pour utiliser le deserializer en créant un Normalizer
+
+            if (isset($json["nom"]) && gettype($json["nom"]) == "string") {
+                $joueur->setNom($json["nom"]);
+            }
+
+            if (isset($json["prenom"]) && gettype($json["prenom"]) == "string") {
+                $joueur->setPrenom($json["prenom"]);
+            }
+
+            if (isset($json["equipe"])) {
+                $idEquipe = $json["equipe"];
+
+                if (gettype($idEquipe) == "integer") {
+                    $equipe = $equipeRepo->find($idEquipe);
+
+                    if ($equipe !== null) { $joueur->setEquipe($equipe); }
+
+                } elseif ($idEquipe == "null") {
+                    $joueur->setEquipe(null);
+                }
+            }
+
+        } catch (Exception $e) {
+            return $this->renvoiJson(Response::HTTP_INTERNAL_SERVER_ERROR, $e);
+        }
+
+        $em->persist($joueur);
+        $em->flush();
+
         $joueurJson = $serializer->serialize($joueur, 'json', ['groups' => 'joueur']);
+
 
         return new JsonResponse($joueurJson, Response::HTTP_OK, [], true);
     }
 
 
-    #[Route('/delete/{id}', name: 'delete_joueur', methods: ['GET'])]
-    public function delete(EntityManagerInterface $entityManager, JoueursRepository $joueursRepository, int $id): Response
+    // Pour rendre le code plus propre
+    private function renvoiJson(int $statut, ?string $details): JsonResponse
     {
-        $joueur = $joueursRepository->find($id);
-
-        $entityManager->remove($joueur);
-        $entityManager->flush();
-
-        return new Response(Response::HTTP_ACCEPTED, Response::HTTP_ACCEPTED, [], true);
-    }
+        return new JsonResponse(
+            json_encode([
+                "statut" => $statut,
+                "details" => $details
+            ]),
+            $statut,
+            [],
+            true
+        );
+    } 
 }
