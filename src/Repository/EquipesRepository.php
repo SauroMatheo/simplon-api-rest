a2b5465088af03d4ea4ffa3446c07b9c0b182672
SauroMatheo <sauromatheo@gmail.com>
2024-11-22 15:46:15 +0100

    Début Recherche-Limite Repo Equipes

--- src/Repository/EquipesRepository.php
+++ src/Repository/EquipesRepository.php
@@ -16,6 +16,73 @@ class EquipesRepository extends ServiceEntityRepository
         parent::__construct($registry, Equipes::class);
     }
 
+    /**
+    * Limite le nombre de résultats d'équipes.
+    * @param int $limit
+    * @param ?int $offset = 0
+    * @return Equipes[] Renvoie un array d'Equpes
+    */
+    public function findLimit(
+        int $limit,
+        ?int $offset = 0
+        ): array
+    {
+        return $this->createQueryBuilder('e')
+            ->setMaxResults(max(1,$limit))
+            ->setFirstResult(max(0, $offset))
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+    * Permet de rechercher des équipes en fonction de leur nom et/ou score minimal/maximal.
+    * Lorsqu'un paramètre est vide, il n'est pas pris en compte, pour faciliter l'utilisation.
+    * @param string     $nom
+    * @param ?int       $scoreMin
+    * @param ?int       $scoreMax
+    * @param ?int       $limit
+    * @param ?int       $offset = 0
+    * @return Equipes[] Returns an array of Articles objects
+    */
+    public function findSearch(
+        string $nom,
+        ?int $scoreMin,
+        ?int $scoreMax,
+        ?int $limit,
+        ?int $offset = 0
+        ): array
+    {
+        $query = $this->createQueryBuilder('e');
+
+        if (!empty($nom)) {
+            $query = $query
+            ->andWhere('e.nom LIKE :nom')
+            ->setParameter('nom', '%'.$nom.'%');
+        }
+
+        if (!empty($scoreMin)) {
+            $query = $query
+            ->andWhere('e.score >= :scoreMin')
+            ->setParameter('scoreMin', $scoreMin);
+        }
+
+        if (!empty($scoreMax)) {
+            $query = $query
+            ->andWhere('e.score <= :scoreMax')
+            ->setParameter('scoreMax', $scoreMax);
+        }        
+        
+        if (!empty($limit)) {
+            $query = $query
+            ->setMaxResults(max(1,$limit));
+        }
+
+        $query = $query->setFirstResult(max(0, $offset));
+
+        return $query->getQuery()->getResult();
+    }
+
 
     // public function findById($id): ?Equipes
     // {
